cb294287e7ee3622006c0d6b1396a7020cc5e724
simcap <simcap@fastmail.com>
2014-12-03 16:59:48 +0100

    With if

--- lib/wiki_footnotes.ex
+++ lib/wiki_footnotes.ex
@@ -1,9 +1,15 @@
 defmodule WikiFootnotes do
 
-  def fetch(url)
+  def fetch(page) do
+    if Regex.match?(~r/http:/, page) do 
+      _fetch(page) 
+    else
+      page |> wikipedia_url |> _fetch
+    end
+  end
 
-  def fetch(theme) do
-    wikipedia_url(theme)
+  def _fetch(url) do
+    url
       |> HTTPoison.get
       |> handle_response
       |> IO.puts
@@ -11,15 +17,14 @@ defmodule WikiFootnotes do
 
   defp handle_response({:ok, %{status_code: 200, body: body}}), do: body
   defp handle_response({:error, %{id: _, reason: reason}}), do: reason 
-
   defp handle_response({:ok, %{status_code: 301, headers: headers}}) do
-    new_theme = headers["Location"] |> String.split("/") |> List.last
-    fetch(new_theme)
+    _fetch(headers["Location"])
+  end
+  defp handle_response({:ok, %{status_code: status, headers: headers}}) do
+    "Unexpected #{status} http code\n#{IO.inspect(headers)}"
   end
 
-  defp handle_response({:ok, %{status_code: status, headers: headers}}), do: "Unexpected #{status} http code\n#{IO.inspect(headers)}"
-
-  defp wikipedia_url(theme) do
-    "http://en.wikipedia.org/wiki/#{theme}"
+  defp wikipedia_url(subject) do
+    "http://en.wikipedia.org/wiki/#{subject}"
   end
 end
